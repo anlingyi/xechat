1453bc31a2318d7e743f2b6cbd274a519e4f3673
yanpanyi <1090172196@qq.com>
2019-03-18 21:00:45 +0800

    创建项目

---
+++ src/main/java/cn/xeblog/xechat/XechatApplication.java
@@ -0,0 +1,13 @@
+package cn.xeblog.xechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XechatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XechatApplication.class, args);
+    }
+
+}

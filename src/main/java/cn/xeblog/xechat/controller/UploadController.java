fd5f32487fa98aec3dfc4de6f888d27faa733bc2
yanpanyi <1090172196@qq.com>
2019-03-29 14:18:00 +0800

    修改上传文件大小配置

--- src/main/java/cn/xeblog/xechat/controller/UploadController.java
+++ src/main/java/cn/xeblog/xechat/controller/UploadController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * 上传文件
--- src/main/java/cn/xeblog/xechat/exception/handler/ExceptionHandle.java
+++ src/main/java/cn/xeblog/xechat/exception/handler/ExceptionHandle.java
@@ -4,29 +4,26 @@ import cn.xeblog.xechat.domain.vo.ResponseVO;
 import cn.xeblog.xechat.enums.CodeEnum;
 import cn.xeblog.xechat.exception.ErrorCodeException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * 异常处理
  *
  * @author yanpanyi
  */
-@ControllerAdvice
 @Slf4j
+@RestControllerAdvice
 public class ExceptionHandle {
 
     @ExceptionHandler(value = Exception.class)
-    @ResponseBody
-    public ResponseVO handle(Exception e) {
+    public ResponseVO exceptionHandler(Exception e) {
         log.error("error:", e);
         return new ResponseVO(CodeEnum.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = ErrorCodeException.class)
-    @ResponseBody
-    public ResponseVO invalidParameterErrorHandler(ErrorCodeException e) {
+    public ResponseVO errorCodeHandler(ErrorCodeException e) {
         return new ResponseVO(e.getCode());
     }
 
